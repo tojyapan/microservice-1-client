c7662aa6863953a6ed76ab798c26c7366d85c095
tojyapan <tojyapan925@gmail.com>
2022-05-05 16:53:45 +0900

    Finish: A Mini-Microservice App

--- src/PostList.js
+++ src/PostList.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import CommentCreate from './CommentCreate';
-import CommentList from './CommentList';
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get('http://localhost:4002/posts');
+    const res = await axios.get("http://localhost:4002/posts");
     setPosts(res.data);
   };
 
@@ -16,27 +16,27 @@ const PostList = () => {
     fetchPosts();
   }, []);
 
-  const renderedPosts = Object.values(posts).map(post => {
+  const renderedPosts = Object.values(posts).map((post) => {
     return (
-      <div 
-        className='card' 
-        style={{ width: '30%', marginBottom: '20px'}}
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
         key={post.id}
       >
-        <div className='card-body'>
+        <div className="card-body">
           <h3>{post.title}</h3>
           <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
-    )
-  })
+    );
+  });
 
   return (
-    <div className='d-flex flex-row flex-wrap justify-content-between'>
+    <div className="d-flex flex-row flex-wrap justify-content-between">
       {renderedPosts}
     </div>
   );
-}
+};
 
 export default PostList;
